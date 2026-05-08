c9255491453d7eee42af67f5ea92844c78af42a5
iamericmin <ericmin3627@gmail.com>
2026-05-08 09:43:42 -0400

    yuh

--- lib/display/visualizers.h
+++ lib/display/visualizers.h
@@ -2,8 +2,8 @@
 #include "fft.h"
 #include "lissajous.h"
 
-#define WAVE_L_COLOR 0x06d4
-#define WAVE_R_COLOR 0x04df
+#define WAVE_L_COLOR 0xFFFF
+#define WAVE_R_COLOR 0x04DF
 #define FFT_L_COLOR_DARK 0x0600
 #define FFT_R_COLOR_DARK 0x05FF
 #define FFT_L_COLOR_LIGHT 0x8FF1
